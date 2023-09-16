3f916de16509216b87a60577c02d5844e2907408
IvanUA <vanylinss94@gmail.com>
2023-09-16 14:47:38 +0300

    fav in progres

--- src/components/Favourite/Favourite.jsx
+++ src/components/Favourite/Favourite.jsx
@@ -5,32 +5,29 @@ import Star from '../STAR/Star'
 
 function Favourite() {
     const [favorites, setFavorites] = useState([]);
-    
+
 
     const toggleFavorite = (favouriteItem) => {
         if (favorites.includes(favouriteItem)) {
-          setFavorites(favorites.filter((item) => item !== favouriteItem));
+            setFavorites(favorites.filter((item) => item !== favouriteItem));
         } else {
-          setFavorites([...favorites, favouriteItem]);
+            setFavorites([...favorites, favouriteItem]);
         }
-      };
+    };
 
     return (
-    <>
-        <div>
-      <h1>Favorite Items</h1>
-      <Star itemName="Item 1" onClick={toggleFavorite} />
-      <Star itemName="Item 2" onClick={toggleFavorite} />
-      {/* Add more items as needed */}
-      <div>
-        <h2>Favorites:</h2>
-        <ul>
-          {favorites.map((favorite) => (
-            <li key={favorite}>{favorite}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+        <>
+            <div>
+                <h1>Favorite Items</h1>
+                <div>
+                    <h2>Favorites:</h2>
+                    <ul>
+                        {favorites.map((favorite) => (
+                            <li key={favorite}>{favorite}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
