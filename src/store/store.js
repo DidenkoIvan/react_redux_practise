093659bc179b72c03db527639cacb9c320729e73
IvanUA <vanylinss94@gmail.com>
2023-09-29 13:46:32 +0300

    first commit in github

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { reducer as favouriteReducer } from "./favourites/favourites.slice";
 
+
 const reducers = combineReducers({
     favourites: favouriteReducer,
 })
